74a082c2ad5f518e28f147a603e21fc9a478d942
Progromasha <carroty1992@gmail.com>
2018-05-24 22:14:43 +0300

    function

---
+++ script.js
@@ -0,0 +1,21 @@
+function create_card(x1) {
+    let x = "карточку";
+    console.log("создаю " + x + " про " + x1);
+    console.log("заполняю " + x + " про " + x1);
+}
+
+create_card("воду");
+
+create_card("свет");
+
+create_card("ток))");
+
+create_card("jkvfldkj");
+
+console.log("x=" + x);
+let s = "x=" + x;
+
+x = x+1;
+
+console.log(s);
+
